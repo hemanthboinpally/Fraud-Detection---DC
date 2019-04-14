ef0bb43b6605856a9d0d0e36682446ac484b529e
hemanthboinpally <hemanth.boinpally@gmail.com>
2019-04-13 18:09:36 -0700

    Implement Logistic Regression

--- frauddetection.py
+++ frauddetection.py
@@ -24,3 +24,36 @@ X, y = prep_data(df)
 
 # Plot our data by running our plot data function on X and y
 plot_data(X, y)
+
+
+#Applying SMOTE
+#In this exercise, you're going to re-balance our data using the Synthetic Minority Over-sampling Technique (SMOTE).
+# Unlike ROS, SMOTE does not create exact copies of observations, but creates new, synthetic,
+# samples that are quite similar to the existing observations in the minority class. SMOTE is t
+# herefore slightly more sophisticated than just copying observations, so let's apply SMOTE to our credit card data
+
+# Get the mean for each group
+df.groupby(by='Class').mean()
+
+# Implement a rule for stating which cases are flagged as fraud
+df['flag_as_fraud'] = np.where(np.logical_and(df['V1']<-3,df['V3']<-5), 1, 0)
+
+# Create a crosstab of flagged fraud cases versus the actual fraud cases
+print(pd.crosstab(df.Class, df.flag_as_fraud, rownames=['Actual Fraud'], colnames=['Flagged Fraud']))
+
+# Create the training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.3, random_state=0)
+
+
+#MML - Logistic Regression
+# Fit a logistic regression model to our data
+model = LogisticRegression()
+model.fit(X_train,y_train)
+
+# Obtain model predictions
+predicted = model.predict(X_test)
+
+# Print the classifcation report and confusion matrix
+print('Classification report:\n', classification_report(y_test,predicted))
+conf_mat = confusion_matrix(y_true=y_test, y_pred=predicted)
+print('Confusion matrix:\n', conf_mat)
