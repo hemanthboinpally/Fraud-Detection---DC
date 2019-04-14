9c5b237f465c9f5ccb73e1815936f1f148830896
hemanthboinpally <hemanth.boinpally@gmail.com>
2019-04-13 17:25:30 -0700

    plot the data

--- frauddetection.py
+++ frauddetection.py
@@ -11,3 +11,16 @@ print(occ)
 
 # Print the ratio of fraud cases
 print(occ / len(df))
+
+# Define a function to create a scatter plot of our data and labels
+def plot_data(X, y):
+	plt.scatter(X[y == 0, 0], X[y == 0, 1], label="Class #0", alpha=0.5, linewidth=0.15)
+	plt.scatter(X[y == 1, 0], X[y == 1, 1], label="Class #1", alpha=0.5, linewidth=0.15, c='r')
+	plt.legend()
+	return plt.show()
+
+# Create X and y from the prep_data function
+X, y = prep_data(df)
+
+# Plot our data by running our plot data function on X and y
+plot_data(X, y)
