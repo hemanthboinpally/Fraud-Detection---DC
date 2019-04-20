c131acfe373328dab3cc657a5dd4050c172bbc71
hemanthboinpally <hemanth.boinpally@gmail.com>
2019-04-20 16:18:16 -0700

    Ensemble model

--- frauddetection.py
+++ frauddetection.py
@@ -173,3 +173,22 @@ CV_model = GridSearchCV(estimator=model, param_grid=param_grid, cv=5, scoring='r
 # Fit the model to our training data and obtain best parameters
 CV_model.fit(X_train, y_train)
 CV_model.best_params_
+
+
+
+## Ensemble Model
+
+# Import the package
+from sklearn.ensemble import VotingClassifier
+
+# Define the three classifiers to use in the ensemble
+clf1 = LogisticRegression(class_weight={0:1, 1:15}, random_state=5)
+clf2 = RandomForestClassifier(class_weight={0:1, 1:12}, criterion='gini', max_depth=8, max_features='log2',
+            min_samples_leaf=10, n_estimators=30, n_jobs=-1, random_state=5)
+clf3 = DecisionTreeClassifier(random_state=5, class_weight="balanced")
+
+# Combine the classifiers in the ensemble model
+ensemble_model = VotingClassifier(estimators=[('lr', clf1), ('rf',clf2), ('dt', clf3)], voting='hard')
+
+# Get the results
+get_model_results(X_train, y_train, X_test, y_test, ensemble_model)
