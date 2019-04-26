8728d0bb73fb35e274a8907d6948f19c08b79dc6
hemanthboinpally <hemanth.boinpally@gmail.com>
2019-04-25 23:24:11 -0700

    Create dictionary and corpus

--- textual_data.py
+++ textual_data.py
@@ -2,4 +2,33 @@
 mask = df['clean_content'].str.contains('sell enron stock', na=False)
 
 # Select the data from df using the mask
-print(df.clean_content[mask])
+print(df.loc[mask])
+
+# Create a list of terms to search for
+searchfor = ['enron stock', 'sell stock', 'stock bonus',  'sell enron stock']
+
+# Filter cleaned emails on searchfor list and select from df
+filtered_emails = df.loc[df['clean_content'].str.contains('|'.join(searchfor), na=False)]
+print(filtered_emails)
+
+# Create flag variable where the emails match the searchfor terms
+df['flag'] = np.where((df['clean_content'].str.contains('|'.join(searchfor)) == True), 1, 0)
+
+# Count the values of the flag variable
+count = df['flag'].value_counts()
+print(count)
+
+
+# Import the packages
+import gensim
+from gensim import corpora
+
+# Define the dictionary
+dictionary = corpora.Dictionary(text_clean)
+
+# Define the corpus
+corpus = [dictionary.doc2bow(text) for text in text_clean]
+
+# Print corpus and dictionary
+print(dictionary)
+print(corpus)
